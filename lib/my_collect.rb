7b0b8a0331b89e113d7c557946d3fc51db0939e6
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-16 02:55:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 
 def my_collect(collection)
   i = 0
+  final_array = []
   if block_given?
     while i < collection.length
-    yield(collection[i])
+    final_array << yield(collection[i])
   i += 1
     end
   end
