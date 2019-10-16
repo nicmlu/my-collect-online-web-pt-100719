9ddf25a770599bc523812fb6f72b973ddb876900
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-16 02:56:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
-
+def my_collect(collection)
+  i = 0
+  final_array = []
+  if block_given?
+    while i < collection.length
+    final_array << yield(collection[i])
+  i += 1
+    end
+  end
+  final_array
+end
